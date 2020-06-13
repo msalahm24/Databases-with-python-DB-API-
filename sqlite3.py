8a43e469eb5daff57ddc52388f7ea3bc817e9356
Mahmoud salah Mohamed <44245137+mahmoud24598salah@users.noreply.github.com>
2020-06-13 19:12:23 +0200

    Update sqlite3.py

--- sqlite3.py
+++ sqlite3.py
@@ -1,16 +1,30 @@
 # first we need to import our sqlite3 from DB API 
 #DB API IS interface for managing the diffrent databases mudules
 import sqlite3
-conn=sqlite3.connect('students')
-# we need to declaring cursor to query row by row
-cursor=conn.cursor()
-query='''
-select * 
-from students
-'''
-#executing our query
-cursor.execute(query)
-rows=cursor.fetchall()
-# rows is list of all our row result query table
-for row in rows:
-    print(f'the first name id {row[0]}')
+def select(query,db):
+    conn=sqlite3.connect(db)
+    # we need to declaring cursor to query row by row
+    cursor=conn.cursor()
+    query=query
+    #executing our query
+    try:
+        cursor.execute(query)
+        rows=cursor.fetchall()
+    except DatabaseError:
+        print('check your database connection')
+    finally:
+        conn.close()
+    # rows is list of all our row result query table
+    return rows
+def insertnewrow(query,db):
+    conn=sqlite3.connect(db)
+    cursor=conn.cursor()
+    query=query
+    try:
+        cursor.except(query)
+        # Atomicity :- the transaction happens as a whole or not at all
+        conn.commit()
+    except DatabaseError:
+        print('check your connection')
+    finally:
+        conn.close()
